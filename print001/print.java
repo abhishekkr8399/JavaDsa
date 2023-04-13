05ff6af8d74ddd23e1e693ce6296754af0819517
ABHISHEK KUMAR <108868672+abhishekkr8399@users.noreply.github.com>
2023-04-13 10:36:20 +0530

    Delete print001 directory

--- print001/print.java
+++
@@ -1,9 +0,0 @@
-package print001;
-//import java.util.*;
-public class print{
-    public static void main(String[] args){
-        System.out.print("Hello World\n");
-        System.out.println("Hello World");
-        System.out.print("Hello World");
-    }
-}
